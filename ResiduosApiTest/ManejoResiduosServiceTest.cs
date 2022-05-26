cdef790e43585745dd0c826fbe16c69139fb3183
Your Name <you@example.com>
2022-05-25 22:59:13 -0500

    Pruebas unitarias

---
+++ ResiduosApiTest/ManejoResiduosServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using API_REPAREMOS.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResiduosApiTest
+{
+    class ManejoResiduosServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task ObtenerResiduosTest()
+        {
+            ManejoResiduosService service = new ManejoResiduosService();
+            var result = await service.ObtenerResiduos();
+            if (result != null)
+            {
+                Assert.IsNotNull(result);
+            }
+            else
+            {
+                Assert.IsNull(result);
+
+            }
+            
+        }
+
+    }
+}
